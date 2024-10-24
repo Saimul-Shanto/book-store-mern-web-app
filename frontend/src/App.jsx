81e034a7e106bbc456ee623f00525f64562dce21
Saimul-Shanto <saimulhaque@iut-dhaka.edu>
2024-10-24 15:00:39 +0600

    create a banner section in home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <Navbar/>
       {/* outlet will render the child routes */}
-      <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
+      <main className='min-h-screen max-w-screen-2xl mx-auto md:px-24 px-4 py-6 font-primary'>
         <Outlet/>
       </main> 
       <footer>Footer</footer>
